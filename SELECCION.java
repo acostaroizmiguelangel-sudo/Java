7961d7738ff84f9b98c59e14d4b64b13796610e6
roizitrus29 <acostaroizmiguelangel@gmail.com>
2025-09-24 23:25:47 -0700

    Seleccion Java
    Miguel Angel Acosta Roiz

---
+++ SELECCION.java
@@ -0,0 +1,26 @@
+public public class SELECCION {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.print("Ya arreglado: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+ SELECCION {
+    
+}
